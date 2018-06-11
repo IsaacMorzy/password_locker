93d0bb9821f0428d7e72fa0a7a2d65365b0f8ce7
Isaac Musyoka <musyokaisaac98@gmail.com>
2018-06-11 10:20:37 +0300

    Add save_user method to save user object into user list

--- userData.py
+++ userData.py
@@ -1,10 +1,18 @@
-from pylint.test.functional.inconsistent_returns import bug_1771_with_user_config
-
 class User:
     user_list = []
+
     def __init__(self, username, password):
         """
         class that generates new instances of users
         """
         self.username = username
         self.password = password
+
+    def save_user(self):
+        """
+        save_user method saves user object into user_list
+        """
+        User.user_list.append(self)
+
+
+    
--- userData_test.py
+++ userData_test.py
@@ -3,16 +3,31 @@ import unittest
 
 class user_test(unittest.TestCase):
     """
-	Test class that defines test cases for the userData class behaviors
-	Args:
-	unittest.TestCase:TestCase class that helps in creating test cases
-	"""
+    Test class that defines test cases for the userData class behaviors
+    Args:
+    unittest.TestCase:TestCase class that helps in creating test cases
+    """
+
+    def test_init(self):
+        """
+        test init test case to test if the object is initialised properly
+        """
+        self.assertEqual(self.new_user.username,"username")
+        self.assertEqual(self.new_user.password, "password")
+
     def setUp(self):
         """
 		set up method to run each test case
 		"""
         self.new_user = User("username", "password")
-
+        
+    def test_create_new_account(self):
+        """
+		test create new account test case to test if the new user object is saved into the user list
+		"""
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
 
 if __name__ == "__main__":
     unittest.main()
+
